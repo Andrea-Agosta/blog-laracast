8a01c98284826847990dc655067d732136abcac3
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-20 13:01:48 +0100

    Section 7 - episode 41

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,21 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::all()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post',[
+        return view('posts.show',[
             'post' => $post
         ]);
     }
