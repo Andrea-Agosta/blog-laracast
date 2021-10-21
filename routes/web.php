f5e697d4612a55f2dbb7c11411db8a9f5f06f6d1
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-21 14:24:57 +0100

    Section 9 - ep.48

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostsController::class, 'show']);
 
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::get('logout', [SessionController::class, 'destroy']);
+
