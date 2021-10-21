efeed42142425af7f4fe4a44ee91523c59eb43ce
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-21 15:39:04 +0100

    Section 9 - ep.50

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+           'email' => 'required|email',
+           'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
-        return redirect('//')->with('success', 'Goodbye!');
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Goodbye!');
     }
 }
