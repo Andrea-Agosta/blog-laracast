98abadbacec9bcdfad1875cdf1f4165b037a2127
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-22 10:35:53 +0100

    Section 10 - ep.52

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,27 @@
+<article class="flex bg-gray-100 p-6 border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="front-bold">Jhon Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+            voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+            occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
+            mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et
+            expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque
+            nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda
+            est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut
+            rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
+            recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+            voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+        </p>
+    </div>
+</article>
