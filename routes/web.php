d9ab9cf3f2e1c3f1d8bf676806d5c903d2d5c9d7
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-25 10:46:18 +0100

    Section 10 - ep.56

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
-Route::get('posts/{post:slug}', [PostsController::class, 'show']);
 
+Route::get('posts/{post:slug}', [PostsController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
@@ -15,5 +17,5 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+Route::get('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
