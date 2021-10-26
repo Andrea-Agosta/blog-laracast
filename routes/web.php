3248cd3f8b383a032981d7b76a1ba1649a7db207
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-26 17:05:54 +0100

    Section 12 - ep.66

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 
-Route::get('logout', [SessionController::class, 'destroy'])->middleware('auth');
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
 Route::get('admin/post/create',[PostsController::class, 'create'])->middleware('admin');
 Route::post('admin/posts',[PostsController::class, 'store'])->middleware('admin');
