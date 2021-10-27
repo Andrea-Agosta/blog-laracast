a8eb8b27d05976edfbc260ac197af241fa83d63d
Andrea Agosta <andrea.agosta85@gmail.com>
2021-10-27 10:53:16 +0100

    Section 12 - ep.67

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminPostController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostsController;
@@ -22,5 +23,10 @@ Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/post/create',[PostsController::class, 'create'])->middleware('admin');
-Route::post('admin/posts',[PostsController::class, 'store'])->middleware('admin');
+// Admin
+Route::post('admin/posts',[AdminPostController::class, 'store'])->middleware('admin');
+Route::get('admin/post/create',[AdminPostController::class, 'create'])->middleware('admin');
+Route::get('admin/posts',[AdminPostController::class, 'index'])->middleware('admin');
+Route::get('admin/posts/{post}/edit',[AdminPostController::class, 'edit'])->middleware('admin');
+Route::patch('admin/posts/{post}',[AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('admin/posts/{post}',[AdminPostController::class, 'destroy'])->middleware('admin');
